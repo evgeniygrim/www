7d873e594f8f242b6facdb739911b9f1c3245091
evgeniygrim <evgeniy.grim@gmail.com>
2014-02-06 12:56:55 +0400

    profile
    верстка

---
+++ profile/scr/spoiler.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+ $('.spoiler_links').click(function(){
+  $(this).parent().children('div.spoiler_body').toggle('normal');
+  return false;
+ });
+});
+$(document).ready(function(){
+ $('.spoiler_links').click(function(){
+  $(this).parent().children('div.spoiler_body1').toggle('normal');
+  return false;
+ });
+});
